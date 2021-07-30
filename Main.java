a9a3b8c529641127d57ba507b425e55cc9e5daee
Aso2001199 <83046918+Aso2001199@users.noreply.github.com>
2021-07-30 16:14:08 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package ship;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("******************");
+        System.out.println("   戦艦ゲーム    ");
+        System.out.println("******************");
+    }
+}
